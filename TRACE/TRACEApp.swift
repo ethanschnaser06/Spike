ae06ad671581dfad8c3af7a1ad719d82301f80ca
ethanschnaser06 <schnaser@wisc.edu>
2020-09-28 20:25:10 -0500

    Initial Commit

---
+++ TRACE/TRACEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRACEApp.swift
+//  TRACE
+//
+//  Created by Ethan Schnaser on 9/28/20.
+//
+
+import SwiftUI
+
+@main
+struct TRACEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
