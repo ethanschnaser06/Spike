3c2bcf589865de8e000a9293169139e4e6d8761e
ethanschnaser06 <schnaser@wisc.edu>
2020-09-28 21:56:37 -0500

    Created AppDelegate(some reason my last commit didnt include this file)

---
+++ TRACE/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  TRACE
+//
+//  Created by Ethan Schnaser on 9/28/20.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
